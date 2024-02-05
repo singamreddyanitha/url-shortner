a2b233af9ccb90df77605551cc0c9ad82205c1ab
Bhargava Teja <nbhargavteja2022@gmail.com>
2024-02-05 18:56:32 +0530

    updated server.js file

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+import connectToDB from "./utils/database.js";
+
 dotenv.config();
 const app = express();
 
@@ -16,23 +18,36 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
-//intialize the server
-const server = app.listen(port, () =>
-  console.log("Server up and running at port:", port)
-);
+// //intialize the server
+// const server = app.listen(port, () =>
+//   console.log("Server up and running at port:", port)
+// );
 
-//listen unhandleRejection
-process.on("unhandledRejection", (reason, p) => {
-  //get slack notification about the error
-  console.error("Unhandled Rejection at:", p, "reason:", reason);
-  server.close();
-  process.exit(1);
-});
-process.on("uncaughtException", (e) => {
-  console.error("Uncaught exception at:", e);
+// //listen unhandleRejection
+// process.on("unhandledRejection", (reason, p) => {
+//   //get slack notification about the error
+//   console.error("Unhandled Rejection at:", p, "reason:", reason);
+//   server.close();
+//   process.exit(1);
+// });
+// process.on("uncaughtException", (e) => {
+//   console.error("Uncaught exception at:", e);
 
-  server.close();
-  process.exit(1);
-});
+//   server.close();
+//   process.exit(1);
+// });
+
+const connect = async () => {
+  try {
+    await connectToDB();
+    app.listen(port, () =>
+      console.log(`Connected to MongoDB & Running on Port ${port}`)
+    );
+  } catch (error) {
+    console.log("Error While Connecting To Mongodb");
+  }
+};
+
+connect();
